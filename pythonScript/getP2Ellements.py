24d7d5c9f7bd67550e80b02c7ee7b70a2cc8aa0c
Binh Bui <binh@vaadin.com>
2017-10-03 12:22:07 +0300

    Fix regex and path

--- pythonScript/getP2Ellements.py
+++ pythonScript/getP2Ellements.py
@@ -4,8 +4,12 @@ from __future__ import print_function
 import pandas as pd
 import urllib
 import simplejson
+import datetime
 import sys
 import re
+
+# Getting today date in format (dd-mm-yy)
+today = datetime.date.today().strftime('%d-%m-%y')
 
 # Read csv from input (input unqualified.csv here)
 df = pd.read_csv(sys.argv[1])
@@ -13,21 +17,21 @@ df = pd.read_csv(sys.argv[1])
 # Make a new csv file for storing updated elements
 # a+: append
 # w+: write (plus means it will create a file if not exist)
-f = open('/Users/nammeo/Desktop/Vaadin/Projects/web-components-testing/generated-files/updatedElements.csv', 'w+')
+f = open('/Users/nammeo/Desktop/Vaadin/Projects/web-components-testing/generated-files/polymer-2-check/updatedElements_%s.csv' % (today), 'a+')
 
 # Define a regex for Polymer 2 version which will match these (>=2.0.0-rc.2 <3.0; ^2.0.0; 1.9 - 2; ^1.0.0 || ^2.0.0, 2.0.0, ^2, ^2.0.2, 2.0.0-rc.3, 1 - 2)
-polymer2Regex = r"(\^2)|(\-\s*2)|(2\.)"
+polymer2Regex = r"(\^2)|(\-\s*2)|([^.]2\.)"
 
 print('There is a total of %d elements to check' % (len(df)))
 
 # Loop through each row
 for index, row in df.iterrows():
 
-	# Define a bool if the element is a Polymer 2 element
+	# Define a bool to detect if the element is a Polymer 2 element
 	p2 = False
 
 	# Skip already read elements (in case errno54: Connection reset by peer)
-	# if index < 572: continue
+	# if index < 545: continue
 
 	# To keep track the progress
 	print('%d %s %s' % (index, row['repo'], row['owner']))
@@ -46,16 +50,16 @@ for index, row in df.iterrows():
 		print('No valid JSON!')
 		continue
 
-
+	# Checking JSON children
 	if 'bower' in jsonData:
 		if 'dependencies' in jsonData['bower']:
 				if 'polymer' in jsonData['bower']['dependencies']:
 					polymerVer = jsonData['bower']['dependencies']['polymer']
 					if re.search(polymer2Regex, polymerVer):
-						if 'version' in jsonData['bower']:
-							f.write(',{0},,{1},{2}\n'.format(row['repo'],row['owner'], jsonData['bower']['version']))
+						if 'version' in jsonData:
+							f.write(',{0},,{1},{2}\n'.format(row['repo'],row['owner'], jsonData['version']))
 						else:
-							f.write(',{0},,{1},{2}\n'.format(row['repo'],row['owner'], row['version']))
+							continue
 						p2 = True
 					else:
 						continue
@@ -67,4 +71,4 @@ for index, row in df.iterrows():
 		continue
 
 	if p2:
-		print('^^^')
+		print(polymerVer)
