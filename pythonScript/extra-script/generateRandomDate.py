4f1ad64fc5ab7e2b381152750745fdeb0528bfb8
Binh Bui <binh@vaadin.com>
2018-03-30 21:12:13 +0300

    Added a script for generating a random date
    The script was created to insert a random date for the comments I made in "All Elements" spreadsheet

--- pythonScript/extra-script/generateRandomDate.py
+++ pythonScript/extra-script/generateRandomDate.py
@@ -1,13 +1,12 @@
 # Python 2.7 or 3 (whatever, I have not tested with 3 yet)
 # Purpose: this script is to generate random unique epoch date for the spreadsheet
-# 29-06-2017 11:35:13 -> 1498736113
-# 06-11-2017 11:35:13 -> 1509968113
-
 import random
 import time
 
 length = 670
+# 29-06-2017 11:35:13 -> 1498736113
 startepoch = 1498736113
+# 06-11-2017 11:35:13 -> 1509968113
 endepoch = 1509968113
 
 randomEpochDates = random.sample(range(startepoch, endepoch), length)
